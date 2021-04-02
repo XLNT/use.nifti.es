8d7679a53ae7786f47fd0ad67a6c62afa82bfea4
Matt Condon (shrugs) <mattgcondon@gmail.com>
2021-04-02 19:09:20 -0400

    fix: allow get,option requests

--- server/lib/cors.ts
+++ server/lib/cors.ts
@@ -1,5 +1,6 @@
 import { NextApiResponse } from 'next';
 
 export function setAllowedOrigins(res: NextApiResponse) {
-  return res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
 }

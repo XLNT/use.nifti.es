e2d591d06f537fc0de425089995e37e4bb04368f
Matt Condon (shrugs) <mattgcondon@gmail.com>
2021-04-02 20:48:12 -0400

    fix: revert to minimum viable cors spec with explicit origin

--- server/lib/cors.ts
+++ server/lib/cors.ts
@@ -1,8 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export function setCors(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,GET');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
 }
